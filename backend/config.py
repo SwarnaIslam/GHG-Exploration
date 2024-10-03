07775ffd148cccb76603aad1c540af18967fe93d
Afia_lubaina <94731125+afia-lubaina@users.noreply.github.com>
2024-10-04 05:36:10 +0600

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -6,3 +6,4 @@ load_dotenv()
 client = MongoClient(os.getenv("MONGODB_URL"))
 db = client["ghg-exploration"]
 users_collection = db["users"]
+GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID", "332332547761-553uld75u11qih884rhfpiesj4u944is.apps.googleusercontent.com")
