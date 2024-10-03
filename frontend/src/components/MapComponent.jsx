d988d198d2a141728f1f2e0aaaec9d880beb2f68
SwarnaIslam <swarna.islam@streamstech.com>
2024-10-04 04:41:33 +0600

    nsac frontend

--- frontend/src/components/MapComponent.jsx
+++ frontend/src/components/MapComponent.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 
 const MapComponent = () => {
-  console.log(import.meta.env.VITE_STREAMLIT_URL);
   return (
     <>
       <iframe
-        src={import.meta.env.VITE_STREAMLIT_URL}
+        src={`${import.meta.env.VITE_STREAMLIT_URL}/?embedded=true`}
         style={{ width: "100vw", height: "100vh", border: "none" }}
         title="Streamlit Dashboard"
       ></iframe>
